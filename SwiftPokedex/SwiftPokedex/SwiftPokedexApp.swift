c940364e53f13f4128e9b7eb69973f22cf29b2d9
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-09-10 16:57:16 -0300

    iniciando a primeira tela

---
+++ SwiftPokedex/SwiftPokedex/SwiftPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPokedexApp.swift
+//  SwiftPokedex
+//
+//  Created by Sthefanny Gonzaga on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
